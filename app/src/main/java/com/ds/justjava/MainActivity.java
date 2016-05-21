0769adccb554c13e74ae09eacab2e692c39fab07
DS <grok.ksl@gmail.com>
2016-05-21 10:52:09 +0530

    Complete project

--- app/src/main/java/com/ds/justjava/MainActivity.java
+++ app/src/main/java/com/ds/justjava/MainActivity.java
@@ -13,23 +13,40 @@ import java.text.NumberFormat;
 
 public class MainActivity extends ActionBarActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        display(1);
-        displayPrice(2 * 5);
+
+
+    //This method is called when the + button is clicked
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+    //This method is called when the + button is clicked
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    //This method is called when the order button is clicked.
+    public void submitOrder(View view) {
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank You!";
+        displayMessage(priceMessage);
+    }
+
+
+
+
+    //This method displays the given quantity value on the screen.
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
@@ -40,4 +57,11 @@ public class MainActivity extends ActionBarActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+    //This method displays the given text on the screen.
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
 }
